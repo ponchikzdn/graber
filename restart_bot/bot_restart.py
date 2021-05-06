a7466166eb0581a2756f0acd4657a6baecab05ed
ponchikzdn <56505083+ponchikzdn@users.noreply.github.com>
2021-05-07 02:10:48 +0300

    Update bot_restart.py

--- restart_bot/bot_restart.py
+++ restart_bot/bot_restart.py
@@ -24,7 +24,7 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 keyboard = InlineKeyboardMarkup(row_width=3)
-donor_k = InlineKeyboardButton(text='@mudak ,', callback_data="donor")
+donor_k = InlineKeyboardButton(text='@mudak ,@of4chan', callback_data="donor")
 moder_k = InlineKeyboardButton(text='@perfectmoder', callback_data="moder")
 channel_k = InlineKeyboardButton(text='@perfekt_world', callback_data="channel")
 keyboard.add(donor_k, moder_k, channel_k)
